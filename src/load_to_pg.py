1b12d20a986402b39490bd7ebcf58b26c7b984fd
ruthasoll <ruths9317@gmail.com>
2026-01-20 22:12:14 +0300

    load json to database

--- src/load_to_pg.py
+++ src/load_to_pg.py
@@ -28,3 +28,22 @@ cur.execute("""
     );
 """)
 conn.commit()
+
+def load_json_to_db(json_path):
+    with open(json_path, 'r') as f:
+        data = json.load(f)
+    for msg in data:
+        cur.execute("""
+            INSERT INTO raw.telegram_messages VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+        """, (msg['message_id'], msg['channel_name'], msg['message_date'], msg['message_text'],
+              msg['has_media'], msg['image_path'], msg['views'], msg['forwards']))
+    conn.commit()
+
+# Example: Load all JSON from a directory
+for root, dirs, files in os.walk('data/raw/telegram_messages'):
+    for file in files:
+        if file.endswith('.json'):
+            load_json_to_db(os.path.join(root, file))
+print(f"Loaded {file} into database.")  
+cur.close()
+conn.close()
